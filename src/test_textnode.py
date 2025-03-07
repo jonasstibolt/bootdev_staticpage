5bc31fabd15cac3ba673a351d3a819b2d6569c55
jonasstibolt <jonasholmgaard@gmail.com>
2025-03-07 08:33:54 +0100

    save point

--- src/test_textnode.py
+++ src/test_textnode.py
@@ -3,4 +3,20 @@ import unittest
 from textnode import TextNode, TextType
 
 class TestTextNode(unittest.TestCase):
-    def test_1(self):
+    def test_eq(self):
+        node1 = TextNode("Hello, World!", TextType.BOLD)
+        node2 = TextNode("Hello, World!", TextType.BOLD)
+        self.assertEqual(node1, node2)
+
+    def test_neq(self):
+        node1 = TextNode("Hello, World!", TextType.BOLD)
+        node2 = TextNode("Hello, World!", TextType.ITALIC)
+        self.assertNotEqual(node1, node2)
+
+    def test_neq2(self):
+        node1 = TextNode("Hello, World!", TextType.BOLD, "https://www.example.com")
+        node2 = TextNode("Hello, World!", TextType.BOLD)
+        self.assertNotEqual(node1, node2)
+
+if __name__ == "__main__":
+    unittest.main()
