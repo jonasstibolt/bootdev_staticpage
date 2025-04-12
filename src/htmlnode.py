7a6b383de98ef6618d8c46a5e355a99280ea81fc
jonasstibolt <jonasholmgaard@gmail.com>
2025-04-12 10:15:34 +0200

    reset

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -10,6 +10,8 @@ class HTMLNode:
         raise NotImplementedError
     
     def props_to_html(self, props):
+        if props is None:
+            return ""
         return " ".join([f'{key}="{value}"' for key, value in props.items()])
     
     def __eq__(self, other):
@@ -17,3 +19,30 @@ class HTMLNode:
     
     def __repr__(self):
         return f"HTMLNode({self.tag}, {self.value}, {self.children}, {self.props})"
+    
+class LeafNode(HTMLNode):
+    def __init__(self, tag=None, value=None, props=None):
+        if value is None:
+            raise ValueError("Leaf nodes must have a value")
+        super().__init__(tag=tag, value=value, props=props)
+        self.children = None
+    
+    def to_html(self):
+        if self.tag == None:
+            return self.value
+        if self.props == None:
+            return f"<{self.tag}>{self.value}</{self.tag}>"
+        return f"<{self.tag} {self.props_to_html(self.props)}>{self.value}</{self.tag}>"
+    
+class ParentNode(HTMLNode):
+    def __init__(self, tag=None, children=None, props=None):
+        if children is None:
+            raise ValueError("Parent nodes must have children")
+        if tag is None:
+            raise ValueError("Parent nodes must have a tag")
+        super().__init__(tag=tag, children=children, props=props)
+
+    def to_html(self):
+        if self.props == None:
+            return f"<{self.tag}>{''.join([child.to_html() for child in self.children])}</{self.tag}>"
+        return f"<{self.tag} {self.props_to_html(self.props)}>{''.join([child.to_html() for child in self.children])}</{self.tag}>"
